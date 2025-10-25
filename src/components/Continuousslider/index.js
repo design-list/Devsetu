e6de27f8e3dd3c2bfe064a77b04b5e8426e75ce5
Personal <sbhupendra6774@gmail.com>
2025-10-25 16:56:02 +0530

    puja page UI done

--- src/components/Continuousslider/index.js
+++ src/components/Continuousslider/index.js
@@ -1,11 +1,11 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules"; // only need Autoplay
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
 
 export default function ContinuousSlider() {
     return (
-        <div className="font-secondary overflow-hidden bg-[var(--color-primary)] font-bold">
+        <div className="font-secondary overflow-hidden bg-[var(--color-primary-light)] font-bold">
             <Swiper
                 modules={[Autoplay]}
                 loop={true}
@@ -17,23 +17,18 @@ export default function ContinuousSlider() {
                     delay: 0,
                     disableOnInteraction: false,
                 }}
-                className="bg-[var(--color-primary)] py-4"
+                className="bg-[var(--color-primary-light)] py-4"
             >
-                <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
+                <SwiperSlide className=" py-5 text-center text-2xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
                   Trusted by Over 1 Million People</SwiperSlide>
-                <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
+                <SwiperSlide className=" py-5 text-center text-2xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
                     100% Secure
                 </SwiperSlide>
-                <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
+                <SwiperSlide className=" py-5 text-center text-2xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
                  Leading Platform for Hindu Devotees Globally
                 </SwiperSlide>
-                 <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
-                  Trusted by Over 1 Million People</SwiperSlide>
-                <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
+                <SwiperSlide className=" py-5 text-center text-2xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
                     100% Secure
-                </SwiperSlide>
-                <SwiperSlide className=" p-5 text-center text-xl capitalize font-medium whitespace-nowrap flex items-center justify-center text-white px-4">
-                 Leading Platform for Hindu Devotees Globally
                 </SwiperSlide>
             </Swiper>
         </div>
